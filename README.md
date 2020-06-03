f49c5f76e68ef0c303fc931cb6729e1d056b567f
nickdpz <ndpastranzamora@gmail.com>
2020-06-03 11:54:20 -0500

    Listas en Pug y crud MVC

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Red-Bicicletas
-Este proyecto tiene mapas para una red de bicicletas
+# Mapa de Bicicletas #
+
+Este proyecto es una plataforma para la gestion de información para biciusuarios
+
+### En que consiste ? ###
+
+* Mapas para la gestión de de rutas de biciusuarios.
+* 1.0.1
+
+### How do I get set up? ###
+ 
+* Express, Pug, Mongo DB
+* Mongo DB Atlas
+* Desarollo: `$ npm run dev`
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const bicicletasAPIRouter = require('./routes/api/bicicletas')
+const bicicletasRouter = require('./routes/bicicletas')
 
-let app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bicicletas',bicicletasRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
