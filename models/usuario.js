bf99bd398a94c18a1ccada7c2f302f515284d370
nickdpz <ndpastranzamora@gmail.com>
2020-06-17 10:01:29 -0500

    Campos de Token a  modelo de usuarios

--- models/usuario.js
+++ models/usuario.js
@@ -1,17 +1,56 @@
 const mongoose = require('mongoose');
 const Reserva = require('./reserva');
+const crypto = require("crypto");
+const Token = require("./token");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const Schema = mongoose.Schema;
 
+
 const usuarioSchema = new Schema({
-    nombre: String,
+    nombre: {
+        type: String,
+        trin: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        trin: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        validate: [validateEmail, 'Por favor ingresar un email válido'],
+        match:  [/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado:{
+        type: Boolean,
+        default: false
+    }
 });
 
-usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
+usuarioSchema.pre('save', (next)=>{
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = (password)=>{
+    return bcrypt.compareSync(password,this.password);
+}
+
+usuarioSchema.methods.reservar = (biciId, desde, hasta, cb) =>{
     const reserva = new Reserva({ usuario: this._id, bicicleta: biciId, desde: desde, hasta: hasta });
     reserva.save(cb);
 }
 
-usuarioSchema.statics.reserva = function (userId) {
+usuarioSchema.statics.reserva = (userId) =>{
     return new Promise(async (resolve, reject) => {
         Reserva.find({ usuario: userId })
             .populate('bicicleta')
